7123415667e766e67eb42e62ac9ff6a0d897f158
Alumno <Alumno@DESKTOP-HR4LLEK>
2018-05-11 12:59:00 -0500

    Primer Proyecto Y Cambio uno

--- UPeUTextX/src/main/java/pe/edu/upeu/upeutextx/Prueba.java
+++ UPeUTextX/src/main/java/pe/edu/upeu/upeutextx/Prueba.java
@@ -5,15 +5,21 @@
  */
 package pe.edu.upeu.upeutextx;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Alumno
  */
 public class Prueba {
-    
+private static final Logger LOGGER = Logger.getLogger( Prueba.class.getName() );    
     
 public static void main(String[] a){
-    System.out.println("Holas Mundo");
+//    System.out.println("Holas Mundo");
+LOGGER.info("Hola Mundo");
+LOGGER.log( Level.FINE, "Hola Mundo");
+
     
 }
     
