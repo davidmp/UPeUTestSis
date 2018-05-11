f58cdc10022c151734909013af67ddcc39c56fff
Alumno <Alumno@DESKTOP-HR4LLEK>
2018-05-11 12:00:59 -0500

    Primer Proyecto Y

---
+++ UPeUTextX/src/main/java/pe/edu/upeu/upeutextx/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.upeutextx;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Prueba {
+    
+    
+public static void main(String[] a){
+    System.out.println("Holas Mundo");
+    
+}
+    
+}
